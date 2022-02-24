54f6ff80dcfc5e16facb0114a54b2f95bd452924
kaushal bhatt <k.bhatt@boutiqaat.com>
2022-02-24 18:47:12 +0530

    optimizeTheSolution

--- NumberOfDigitsInNumber.java
+++ NumberOfDigitsInNumber.java
@@ -2,14 +2,17 @@ public class NumberOfDigitsInNumber{
 
     public static void main(String[] args) {
         
-        System.out.println(CountDigits(100));
+        System.out.println(CountDigits(1234567890));
     }
     static int CountDigits(int number){
-        int count=0;
-        while(number!=0){
-            number=number/10;
-            ++count;
-        }
-        return  count;
+        // int count=0;
+        // while(number!=0){
+        //     number=number/10;
+        //     ++count;
+        // }
+        // return  count;
+        return (int) (Math.log10(number)+1);
+        //this formula give answers in constant time
+        // increasing its time complexity
     }
 }
