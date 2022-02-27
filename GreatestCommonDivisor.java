29c46ce57a8a5ab40850bb3b5fd339ea0861c387
kaushal bhatt <k.bhatt@boutiqaat.com>
2022-02-28 00:41:34 +0530

    optimized solution

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -10,13 +10,22 @@ public class GreatestCommonDivisor{
      * @param secondNumber
      * @return
      */
+    // public static int solution(int firstNumber,int secondNumber){
+    //     while(firstNumber!=secondNumber){
+    //         if(firstNumber>secondNumber)
+    //         firstNumber=firstNumber-secondNumber;
+    //         else
+    //         secondNumber=secondNumber-firstNumber;
+    //     }
+    //     return firstNumber;
+    // }
+
+    //best solution is below one above one is good but not the best so check this one
     public static int solution(int firstNumber,int secondNumber){
-        while(firstNumber!=secondNumber){
-            if(firstNumber>secondNumber)
-            firstNumber=firstNumber-secondNumber;
-            else
-            secondNumber=secondNumber-firstNumber;
-        }
+        if(secondNumber==0)
         return firstNumber;
+        else
+            return solution(secondNumber,firstNumber%secondNumber);
     }
+
 }
