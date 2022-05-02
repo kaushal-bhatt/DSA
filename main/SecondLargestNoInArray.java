23cf6d98fa7879d4f12db39afee7df454dd32cae
kaushal bhatt <k.bhatt@boutiqaat.com>
2022-05-02 15:10:02 +0530

    understand how to find second largest completely today

--- main/SecondLargestNoInArray.java
+++ main/SecondLargestNoInArray.java
@@ -1,41 +1,28 @@
 package main;
 
-
-
 public class SecondLargestNoInArray {
-	public static void main(String[] args) {
-		long a = System.currentTimeMillis();
-		
-		int[] arr = {10,100, 6, 6};
-		System.out.println("First Large No Index is : "+largest(arr));
-		long b = System.currentTimeMillis();
-		System.out.println("Second Largest NO Index is :" + SecondLargest(arr));
-		System.out.println(b - a + " sec");
-		
-	}
-	
-	static int largest(int[] arr) {
-		int largest = 0;
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] > arr[largest]) {
-				largest = i;
-			}
+		public static void main(String[] args) {
+			int [] arr ={4,2,3,6,7,9,8};
+			int Secondlargest=fetchSecondLargest(arr);
+			System.out.println(arr[Secondlargest]);
 		}
-		return largest;
-	}
-	static int SecondLargest(int [] arr){
-		int result=-1;
-		for(int i=0;i<arr.length;i++){
-			if (arr[i] != arr[largest(arr)]) {
-				if(result==-1){
-					result=i;
+		static int fetchSecondLargest(int [] arr){
+			int largest=0;// 3 4 5
+			int result=-1;// 1 2 0 3 4 6
+			for(int i=1;i<arr.length;i++){
+				if(arr[i]>arr[largest]){//checks second element in array is greater then first or not , if true then make assign largest element index to current index i and previous largest assigned to result
+					result=largest;
+					largest=i;
 				}
-				else if(arr[i]>arr[result]){
-					result=i;
+				else if(arr[i]!=arr[largest]){//this line means array is not contains only 1 element
+					if(result==-1 || arr[i]>arr[result]){//if result == -1 means arr[i] is not greater than arr[largest],|| or if arr[i]>arr[result] means next element is greater the previous largest element than only assign result =i
+						result=i;
+					}
 				}
 			}
+			return largest;
 		}
-		return result;
+		
 	}
 
 	
